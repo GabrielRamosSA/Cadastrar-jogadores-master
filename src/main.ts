b3c51ac1afd610cbfc8990920f538e1849db0d51
GabrielRamosSA <gabrielramossantos@live.com>
2024-07-09 09:47:04 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+/// <reference types="@angular/localize" />
+
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { HomeComponent } from './app/home.component';
+
+bootstrapApplication(HomeComponent, appConfig)
+  .catch((err) => console.error(err));
